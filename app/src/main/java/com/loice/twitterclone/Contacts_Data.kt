f9f0a3b22cdabaa09ec9a14ad7fe674abb3591a8
Loice-KaniniMwau <mwauloice@gmail.com>
2023-06-16 11:53:34 +0300

    my contacts app

--- app/src/main/java/com/loice/twitterclone/Contacts_Data.kt
+++ app/src/main/java/com/loice/twitterclone/Contacts_Data.kt
@@ -2,18 +2,11 @@ package com.loice.twitterclone
 
 data class Contacts_Data(
     var avatar:String,
-//    var displayName:String,
-//    var handle:String,
-//    var tweet:String,
-//    var commentCount:Int,
-//    var likeCount:Int,
-//    var rtCount:Int
+
 var contact_name:String,
-    var country_code:String,
-    var phone_number:String,
-    var favourite:Boolean,
-    var contact_type:String,
-    var other_number:String,
+    var email:String,
+    var number:String,
+
 
 
 )
